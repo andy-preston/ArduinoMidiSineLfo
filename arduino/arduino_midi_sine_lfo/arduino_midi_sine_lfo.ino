05932d2d1d9f07e498952849c2f29d85ad9977e5
Andy Preston <edgeeffect@gmail.com>
2016-04-23 14:56:44 +0100

    fade LED using wave table

--- arduino/arduino_midi_sine_lfo/arduino_midi_sine_lfo.ino
+++ arduino/arduino_midi_sine_lfo/arduino_midi_sine_lfo.ino
@@ -1,23 +1,22 @@
 int led = 13;
 int led2 = 11;
-int brightness = 0;
-int fadeAmount = 1;
-int onOff = HIGH;
+int onOff = LOW;
+int index = 0;
 extern int sineTable[];
 
 void setup() {                
   pinMode(led, OUTPUT);     
-  pinMode(led2, OUTPUT);     
+  pinMode(led2, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(led, onOff);
-  onOff = (onOff == HIGH) ? LOW : HIGH;
-  analogWrite(led2, brightness * 2);    
-  brightness = brightness + fadeAmount;
-  if (brightness == 0 || brightness == 127) {
-    fadeAmount = -fadeAmount ; 
-  }
-  delay(50);
+    analogWrite(led2, sineTable[index] * 2);
+    index = index + 1;
+    if (index >= 1023) {
+        index = 0;
+        onOff = (onOff == HIGH) ? LOW : HIGH;
+        digitalWrite(led, onOff);
+    }
+    delay (1);
 }
 
